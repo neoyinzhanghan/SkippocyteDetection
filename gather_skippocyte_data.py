3b21c6d8bfeec6b51049575dc4dbb64de093601c
Neo Yin <yinzhang@berkeley.edu>
2024-05-06 16:28:05 -0700

    first commit

---
+++ gather_skippocyte_data.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import pandas as pd
+from tqdm import tqdm
+
+data_dirs = [
+    "/media/ssd2/dh_labelled_data/DeepHeme1/UCSF_repo",
+    "/media/ssd2/dh_labelled_data/DeepHeme1/MSK_repo_normal",
+    "/media/ssd2/dh_labelled_data/DeepHeme1/MSK_repo_mixed",
+    "/media/ssd2/dh_labelled_data/DeepHeme2/PBS/labelled_cartridge_2",
+    "/media/ssd2/dh_labelled_data/DeepHeme2/PBS/labelled_cartridge_1",
+    "/media/ssd2/dh_labelled_data/DeepHeme2/PBS/labelled_cartridge_0",
+    "/media/ssd2/dh_labelled_data/DeepHeme2/BMA/cartridge_1",
+    "/media/hdd3/neo/LabelledBMASkippocytes",
+]
+save_dir = ""
+
+skipped_classes = ["BLR", "SKP", "ER5", "ER6", "U4", "U1", "PL2", "PL3"]
+
+os.makedirs(save_dir, exist_ok=True)
+os.makedirs(os.path.join(save_dir, "good"), exist_ok=True)
+os.makedirs(os.path.join(save_dir, "bad"), exist_ok=True)
+
+# each data_dir in data_dirs contain a bunch of subdirectories, each containing images, the name of the subdirectory is the label
+# if the label is in skipped_classes, the images are shutil copied to the bad directory DO NOT MOVE THEM!
+# when saving new file, use indexing 0,1,2, ... for the files, and keep a metadata file with the original path and label, and the idx
+
+
+metadata = {
+    "idx": [],
+    "path": [],
+    "label": [],
+    "good": [],
+}
+
+current_idx = 0
+
+for data_dir in tqdm(data_dirs, desc="Processing Data Directories"):
+    for label in tqdm(os.listdir(data_dir), desc="Processing Labels"):
+
+        # if label contains other, Other, the label should be moved to bad
+        if "other" in label.lower():
+            for img in os.listdir(os.path.join(data_dir, label)):
+                old_path = os.path.join(data_dir, label, img)
+                new_path = os.path.join(save_dir, "bad", f"{current_idx}.jpg")
+                shutil.copyfile(old_path, new_path)
+
+                metadata["idx"].append(current_idx)
+                metadata["path"].append(old_path)
+                metadata["label"].append(label)
+                metadata["good"].append(False)
+
+                current_idx += 1
+
+        if label in skipped_classes:
+            for img in os.listdir(os.path.join(data_dir, label)):
+                old_path = os.path.join(data_dir, label, img)
+                new_path = os.path.join(save_dir, "bad", f"{current_idx}.jpg")
+                shutil.copyfile(old_path, new_path)
+
+                metadata["idx"].append(current_idx)
+                metadata["path"].append(old_path)
+                metadata["label"].append(label)
+                metadata["good"].append(False)
+
+                current_idx += 1
+        else:
+            for img in os.listdir(os.path.join(data_dir, label)):
+                old_path = os.path.join(data_dir, label, img)
+                new_path = os.path.join(save_dir, "good", f"{current_idx}.jpg")
+                shutil.copyfile(old_path, new_path)
+
+                metadata["idx"].append(current_idx)
+                metadata["path"].append(old_path)
+                metadata["label"].append(label)
+                metadata["good"].append(True)
+
+                current_idx += 1
+
+metadata_df = pd.DataFrame(metadata)
+
+metadata_df.to_csv(os.path.join(save_dir, "metadata.csv"), index=False)
+print("Done.")
---
+++ train.py
@@ -0,0 +1,121 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms, models, datasets
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torchmetrics import Accuracy, AUROC, F1Score
+
+path_to_your_data = ""
+
+
+def get_feat_extract_augmentation_pipeline(image_size):
+    transform_shape = A.Compose(
+        [
+            A.ShiftScaleRotate(p=0.8),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Affine(shear=(-10, 10), p=0.3),
+            A.ISONoise(
+                color_shift=(0.01, 0.02),
+                intensity=(0.05, 0.01),
+                always_apply=False,
+                p=0.2,
+            ),
+        ]
+    )
+    transform_color = A.Compose(
+        [
+            A.RandomBrightnessContrast(
+                contrast_limit=0.4, brightness_by_max=0.4, p=0.5
+            ),
+            A.CLAHE(p=0.3),
+            A.ColorJitter(p=0.2),
+            A.RandomGamma(p=0.2),
+        ]
+    )
+    return A.Compose(
+        [
+            A.Resize(image_size, image_size),
+            A.OneOf([transform_shape, transform_color]),
+            ToTensorV2(),
+        ]
+    )
+
+
+class BinaryResNet(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = models.resnet50(pretrained=True)
+        num_features = self.model.fc.in_features
+        self.model.fc = torch.nn.Linear(num_features, 1)
+        self.accuracy = Accuracy()
+        self.auroc = AUROC(num_classes=1)
+        self.f1 = F1Score(num_classes=1, threshold=0.5)
+        self.transform = get_feat_extract_augmentation_pipeline(96)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.binary_cross_entropy_with_logits(y_hat.view(-1), y.float())
+        self.log("train_loss", loss)
+        self.log("train_acc", self.accuracy(torch.sigmoid(y_hat), y.int()))
+        self.log("train_auc", self.auroc(torch.sigmoid(y_hat), y.int()))
+        self.log("train_f1", self.f1(torch.sigmoid(y_hat), y.int()))
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.binary_cross_entropy_with_logits(y_hat.view(-1), y.float())
+        self.log("val_loss", loss)
+        self.log("val_acc", self.accuracy(torch.sigmoid(y_hat), y.int()))
+        self.log("val_auc", self.auroc(torch.sigmoid(y_hat), y.int()))
+        self.log("val_f1", self.f1(torch.sigmoid(y_hat), y.int()))
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def prepare_data(self):
+        dataset = datasets.ImageFolder(root=path_to_your_data)
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+        self.train_dataset, self.val_dataset = random_split(
+            dataset, [train_size, val_size]
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=32,
+            shuffle=True,
+            num_workers=4,
+            collate_fn=self.collate_fn,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset, batch_size=32, num_workers=4, collate_fn=self.collate_fn
+        )
+
+    def collate_fn(self, batch):
+        imgs, targets = zip(*batch)
+        imgs = torch.stack([self.transform(image=img)["image"] for img in imgs])
+        targets = torch.tensor(targets)
+        return imgs, targets
+
+
+if __name__ == "__main__":
+    model = BinaryResNet()
+    logger = TensorBoardLogger(save_dir="/path/to/save/logs", name="binary_resnet50")
+    checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min")
+    trainer = Trainer(max_epochs=20, logger=logger, callbacks=[checkpoint_callback])
+    trainer.fit(model)
