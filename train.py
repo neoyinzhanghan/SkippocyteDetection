90cbbfb1aa45208b1c349830b381b683638db462
Neo Yin <yinzhang@berkeley.edu>
2024-05-07 23:20:25 -0700

    skippocytes

--- train.py
+++ train.py
@@ -207,7 +207,7 @@ class ResNetModel(pl.LightningModule):
 # Main training loop
 def train_model(downsample_factor):
     data_module = ImageDataModule(
-        data_dir="/media/hdd3/neo/blasts_skippocytes_split",
+        data_dir="/media/hdd2/neo/blasts_skippocytes_split",
         batch_size=32,
         downsample_factor=downsample_factor,
     )
